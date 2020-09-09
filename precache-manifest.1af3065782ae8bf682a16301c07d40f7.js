47f07f9e8e98ab55ba1aa5c23ae5c3c7b6535f2b
Ilya Mlinarzh <kopasru@gmail.com>
2020-09-09 22:42:19 +0500

    Updates

---
+++ precache-manifest.1af3065782ae8bf682a16301c07d40f7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a04498ebe17e319b386b7afa336b91c",
+    "url": "/react-vezdekod-project-web.github.io/index.html"
+  },
+  {
+    "revision": "d947f4ecbc1b40b2bc3d",
+    "url": "/react-vezdekod-project-web.github.io/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/react-vezdekod-project-web.github.io/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/react-vezdekod-project-web.github.io/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d947f4ecbc1b40b2bc3d",
+    "url": "/react-vezdekod-project-web.github.io/static/js/main.6f18c67d.chunk.js"
+  },
+  {
+    "revision": "a70c886556fb566a17ad",
+    "url": "/react-vezdekod-project-web.github.io/static/js/runtime-main.d00ffe7f.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/react-vezdekod-project-web.github.io/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/react-vezdekod-project-web.github.io/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/react-vezdekod-project-web.github.io/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/react-vezdekod-project-web.github.io/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/react-vezdekod-project-web.github.io/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/react-vezdekod-project-web.github.io/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/react-vezdekod-project-web.github.io/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/react-vezdekod-project-web.github.io/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/react-vezdekod-project-web.github.io/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/react-vezdekod-project-web.github.io/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/react-vezdekod-project-web.github.io/static/media/sub.4eb05fdd.png"
+  }
+]);
